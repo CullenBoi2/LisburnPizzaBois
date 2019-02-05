7e0de4a840de2a60cd76a06ab4b7ea53f80b2052
Jason Dermot Mcgurran <MCG40070939@serc.ac.uk>
2019-02-05 14:31:42 +0000

    Created frmPizza

---
+++ LisburnPizzabois/FrmPizza.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LisburnPizzabois
+{
+    public partial class FrmPizza : Form
+    {
+        public FrmPizza()
+        {
+            InitializeComponent();
+        }
+
+        //Variable to hold the values
+        public static double dPrice, dSubTotal, dDiscount = 0;
+        public static int dTotal = 0;
+
+        private void pichawaiian_mouseLeave(object sender, EventArgs e)
+        {
+            //code to clear the pizza name and price display
+            Pizzalbl = "";
+        }
+        private void RegularCustomer_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
