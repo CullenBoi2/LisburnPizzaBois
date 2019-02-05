496f94a104cd302c4d7d3bbb6784f2ce364b20ad
Cullen Mc Neice <MCN40057906@serc.ac.uk>
2019-02-05 13:55:18 +0000

    Frm Login created

--- LisburnPizzabois/frmLogin.Designer.cs
+++ LisburnPizzabois/frmLogin.Designer.cs
@@ -28,13 +28,106 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnEnter = new System.Windows.Forms.Button();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(317, 224);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 0;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(407, 224);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 1;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnEnter
+            // 
+            this.btnEnter.Location = new System.Drawing.Point(225, 224);
+            this.btnEnter.Name = "btnEnter";
+            this.btnEnter.Size = new System.Drawing.Size(75, 23);
+            this.btnEnter.TabIndex = 2;
+            this.btnEnter.Text = "Enter";
+            this.btnEnter.UseVisualStyleBackColor = true;
+            this.btnEnter.Click += new System.EventHandler(this.btnEnter_Click);
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(261, 129);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(100, 20);
+            this.txtUsername.TabIndex = 3;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(261, 155);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(100, 20);
+            this.txtPassword.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(200, 132);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Username";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(202, 158);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Password";
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.btnEnter);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Name = "frmLogin";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.frmLogin_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnEnter;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- LisburnPizzabois/frmLogin.cs
+++ LisburnPizzabois/frmLogin.cs
@@ -12,9 +12,80 @@ namespace LisburnPizzabois
 {
     public partial class frmLogin : Form
     {
+        //variables 
+        public static String sname = "";
         public frmLogin()
         {
+             
             InitializeComponent();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            ClearDetails();
+        }
+
+        public void ClearDetails()
+        {
+            //local variable to store result
+            DialogResult dr;
+            //accept response
+            dr = MessageBox.Show("Are you sure you want to exit",
+                "Confirm exit",
+                MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            //if yes then close program
+            if (dr == DialogResult.No)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            //frmPizza sw = new frmPizza(); Remove comment when frmPizza Load
+            this.Hide();
+            // sw.Show();
+        }
+
+        public void Halt()
+        {
+            if (txtUsername.Text == "Admin" && txtPassword.Text == "Pa$$w0rd")
+            {
+                 //frmPizza sw = new frmPizza(); Remove comment when frmPizza load
+                this.Hide();
+                //sw.Show();                  Remove comment when frmPizza load
+            }
+            else
+            {
+                MessageBox.Show("Incorrect Details", "Please re-enter Details",
+                MessageBoxButtons.YesNo, MessageBoxIcon.Hand);
+                ClearDetails();
+
+            }
+
+            //code to check the username and password
+
+          
+        }
+
+        private void btnEnter_Click(object sender, EventArgs e)
+        {
+            //local variable to store result
+            DialogResult dr;
+            //accept response
+            dr = MessageBox.Show("Are you sure you want to exit?",
+                "Confirm Exit",
+            MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            //if yes then close program
+            if (dr == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void frmLogin_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
